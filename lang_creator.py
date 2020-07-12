da44e22b4d7e1101de9c960c964b47578d365b90
JuliusTZM <julius.tzm@gmail.com>
2020-07-12 15:17:34 +0200

    Quest lines has been localized as well

--- lang_creator.py
+++ lang_creator.py
@@ -23,31 +23,50 @@ with open( 'DefaultQuests.json', 'rb' ) as json_file:
     quest_db = json.load( json_file )
     json_file.close()
 
-n = 0
-
 with open( 'en_us.lang', 'w' ) as f, open( '%s' % lang + '.lang', 'w' ) as t:
-    f.write( "# PARSE_ESCAPES\n\n" )
-    t.write( "# PARSE_ESCAPES\n\n" )
+    f.write( "#PARSE_ESCAPES\n\n#Quests\n" )
+    t.write( "#PARSE_ESCAPES\n\n#Quests\n" )
+
+    n = 0
     for quest_db_rec in quest_db['questDatabase:9']:
         n += 1
-        name = quest_db['questDatabase:9'][quest_db_rec]['properties:10']['betterquesting:10']['name:8']
-        desc = quest_db['questDatabase:9'][quest_db_rec]['properties:10']['betterquesting:10']['desc:8']
-        f.write( "mypack.quest%d.name=%s\n" % (n, repr( name )[1:-1]) )
-        f.write( "mypack.quest%d.desc=%s\n" % (n, repr( desc )[1:-1]) )
+        q_name = quest_db['questDatabase:9'][quest_db_rec]['properties:10']['betterquesting:10']['name:8']
+        q_desc = quest_db['questDatabase:9'][quest_db_rec]['properties:10']['betterquesting:10']['desc:8']
+        f.write( "mypack.quest%d.name=%s\n" % (n, repr( q_name )[1:-1]) )
+        f.write( "mypack.quest%d.desc=%s\n" % (n, repr( q_desc )[1:-1]) )
         t.write( "mypack.quest%d.name=\nmypack.quest%d.desc=\n" % (n, n) )
 
-n = 0
+    f.write( "\n#Quest lines\n" )
+    t.write( "\n#Quest lines\n" )
 
-with open( 'tmp.json', 'w' ) as tmp_file:
+    n = 0
+    for quest_lines_rec in quest_db['questLines:9']:
+        n += 1
+        ql_name = quest_db['questLines:9'][quest_lines_rec]['properties:10']['betterquesting:10']['name:8']
+        ql_desc = quest_db['questLines:9'][quest_lines_rec]['properties:10']['betterquesting:10']['desc:8']
+        f.write( "mypack.quest_line%d.name=%s\n" % (n, repr( ql_name )[1:-1]) )
+        f.write( "mypack.quest_line%d.desc=%s\n" % (n, repr( ql_desc )[1:-1]) )
+        t.write( "mypack.quest_line%d.name=\nmypack.quest_line%d.desc=\n" % (n, n) )
+
+n = 0
+with open( 'DefaultQuests.json_new', 'w' ) as tmp_file:
+    n = 0
     for quest_db_rec in quest_db['questDatabase:9']:
         n += 1
         quest_db['questDatabase:9'][quest_db_rec]['properties:10']['betterquesting:10']['name:8'] = "mypack.quest%d" \
                                                                                                     ".name" % n
         quest_db['questDatabase:9'][quest_db_rec]['properties:10']['betterquesting:10']['desc:8'] = "mypack.quest%d" \
                                                                                                     ".desc" % n
+    n = 0
+    for quest_lines_rec in quest_db['questLines:9']:
+        n += 1
+        quest_db['questLines:9'][quest_lines_rec]['properties:10']['betterquesting:10']['name:8'] = "mypack" \
+                                                                                                    ".quest_line%d.name" % n
+        quest_db['questLines:9'][quest_lines_rec]['properties:10']['betterquesting:10']['desc:8'] = "mypack" \
+                                                                                                    ".quest_line%d.desc" % n
     json.dump( quest_db, tmp_file, indent=2, separators=(',', ': '), ensure_ascii=False )
 
-f.close
-t.close
-tmp_file.close
-json_file.close
+f.close()
+t.close()
+tmp_file.close()
+json_file.close()
